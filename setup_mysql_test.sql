3a93b45347aedd48d5849dddbfc9cb82ce147be9
Innocente GIHOZO <i.gihozo@alustudent.com>
2024-01-31 16:33:33 +0200

    mysql test file

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -1,5 +1,9 @@
 --MySQL Test server for the AirBnB project.
 CREATE DATABASE IF NOT EXISTS hbnb_test_db;
+CREATE DATABASE IF NOT EXISTS hbnb_test_db_fake;
+
+
 CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
-GRANT ALL PRIVILEGES ON hbnb_test_db . * TO 'hbnb_test'@'localhost';
-GRANT SELECT ON performance_schema . * TO 'hbnb_test'@'localhost';
+GRANT ALL PRIVILEGES ON hbnb_test_db. * TO 'hbnb_test'@'localhost';
+GRANT SELECT ON performance_schema. * TO 'hbnb_test'@'localhost';
+GRANT ALL PRIVILEGES ON hbnb_test_db_fake.* TO 'hbnb_test'@'localhost';
