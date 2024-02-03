8a8437247be8aa1f77f6f21836272a6e1cbfc2cc
Innocente GIHOZO <i.gihozo@alustudent.com>
2024-02-03 19:18:25 +0200

    Modified codebase

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -1,9 +1,10 @@
 --MySQL Test server for the AirBnB project.
+-- Creates the database hbnb_test_db
 CREATE DATABASE IF NOT EXISTS hbnb_test_db;
-CREATE DATABASE IF NOT EXISTS hbnb_test_db_fake;
-
-
+-- add the user hbnb_test identified with password 'hbnb_test_pwd'
 CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
-GRANT ALL PRIVILEGES ON hbnb_test_db. * TO 'hbnb_test'@'localhost';
-GRANT SELECT ON performance_schema. * TO 'hbnb_test'@'localhost';
-GRANT ALL PRIVILEGES ON hbnb_test_db_fake.* TO 'hbnb_test'@'localhost';
+-- add privileges to hbnb_test
+GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
+GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost';
+-- refresh to apply privileges
+-- FLUSH PRIVILEGES;
