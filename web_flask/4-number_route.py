6c2f93c66e5ff90f74b3b07da6dcdb734b079cb7
gihozoinnocente <88516265+gihozoinnocente@users.noreply.github.com>
2024-02-23 14:21:08 +0200

    4-number_route.py

--- web_flask/4-number_route.py
+++ web_flask/4-number_route.py
@@ -36,7 +36,20 @@ def cisfun(text):
 
 @app.route("/python", strict_slashes=False)
 @app.route("/python/<text>", strict_slashes=False)
-def pythoniscool(text="is cool"):
-    """Displays 'Python', followed by the value of <text>"""
+def python(text="is cool"):
+    """Displays 'Python' followed by the value of <text>.
+
+    Replaces any underscores in <text> with slashes.
+    """
     text = text.replace("_", " ")
-    return "Python {}".format(text)te.p" 52L, 1331C                                                                                                              1,1           Top
+    return "Python {}".format(text)
+
+
+@app.route("/number/<int:n>", strict_slashes=False)
+def isnumber(n):
+    """Displays 'n is a number' only if n is an integer."""
+    return "{} is a number".format(n)
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0")
