d87ef3621214006cc55008e776ee1ceda8f3531d
Innocente GIHOZO <i.gihozo@alustudent.com>
2024-02-24 19:12:56 +0200

    Bug solve number template

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -52,12 +52,11 @@ def isnumber(n):
     return "{} is a number".format(n)
 
 
-@app.route("/number_template/<int:n>", strict_slashes=False)
+@app.route('/number_template/<int:n>', strict_slashes=False)
 def number_template(n):
-    """Displays an HTML page only if <n> is an integer."""
-    return render_template("5-number.html", n=n)
-
-
+    """ display html if n is int. """
+    n = str(n)
+    return render_template('5-number.html', n=n)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0")
